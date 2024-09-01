c111f15b94f3ada34db6e8f54b7876bc6e834c39
Sameer Basha <sameerndl28@gmail.com>
2024-09-01 16:58:32 +0530

    DSA

---
+++ ArrayIntroduction/Qs12_tripletSum_approach1.cpp
@@ -0,0 +1,49 @@
+//question link
+// https://www.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Should return true if there exists a triplet in the
+    // array arr[] which sums to x. Otherwise false
+    bool find3Numbers(int arr[], int n, int x) {
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    if(arr[i]+arr[j]+arr[k]==x)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, X;
+        cin >> n >> X;
+        int i, A[n];
+        for (i = 0; i < n; i++)
+            cin >> A[i];
+        Solution ob;
+        cout << ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
---
+++ ArrayIntroduction/Qs12_tripletSum_approach2.cpp
@@ -0,0 +1,50 @@
+//question link
+// https://www.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=practice_card
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Should return true if there exists a triplet in the
+    // array arr[] which sums to x. Otherwise false
+    bool find3Numbers(int arr[], int n, int x) {
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++)
+        {
+            int j=i+1;
+            int k=n-1;
+            
+            while(j<k)
+            {
+                int sum=arr[i]+arr[j]+arr[k];
+                
+                if(sum==x) return true;
+                else if(sum<x) j++;
+                else k--;
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, X;
+        cin >> n >> X;
+        int i, A[n];
+        for (i = 0; i < n; i++)
+            cin >> A[i];
+        Solution ob;
+        cout << ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
