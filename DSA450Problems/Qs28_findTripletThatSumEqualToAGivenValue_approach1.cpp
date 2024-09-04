e85bc97b517d8242cb4950e63e1eac83ae807ce8
Sameer Basha <sameerndl28@gmail.com>
2024-09-04 15:35:34 +0530

    DSA

---
+++ DSA450Problems/Qs28_findTripletThatSumEqualToAGivenValue_approach1.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Should return true if there exists a triplet in the
+    // array arr[] which sums to x. Otherwise false
+    bool find3Numbers(int arr[], int n, int x) {
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                for(int k=j+1;k<n;k++)
+                {
+                    int sum=arr[i]+arr[j]+arr[k];
+                    if(sum==x) return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n, X;
+        cin >> n >> X;
+        int i, A[n];
+        for (i = 0; i < n; i++)
+            cin >> A[i];
+        Solution ob;
+        cout << ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
