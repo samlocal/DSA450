8baf5c54ea50910f5c6b0b68b9780fc7b76250d5
Sameer Basha <sameerndl28@gmail.com>
2024-09-07 12:10:45 +0530

    Matrix

--- TwoDimensionalArraysIntroduction/Qs2_wavyPrintOfMatrix.cpp
+++ TwoDimensionalArraysIntroduction/Qs2_wavyPrintOfMatrix.cpp
@@ -1,19 +1,23 @@
-
-/*for(int col=0;col<m;col++)
+#include <bits/stdc++.h> 
+using namespace std;
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
 {
-    if(col%2==0)
+    vector<int>ans;
+    for(int col=0;col<mCols;col++)
     {
-        for(int row=0;row<n;row++)
+        if(col%2==0)
         {
-            cout<<matrix[row][col];
+            for(int row=0;row<nRows;row++)
+            {
+                ans.push_back(arr[row][col]);
+            }
+        }
+        else{
+            for(int row=nRows-1;row>=0;row--)
+            {
+                ans.push_back(arr[row][col]);
+            }
         }
     }
-    else{
-        for(int row=n-1;row>=0;row--)
-        {
-            cout<<mat[row][col];
-        }
-    }
-
+    return ans;
 }
-*/
