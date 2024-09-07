dec1434476d162604aa9dbd21a255db5b56f7bdd
Sameer Basha <sameerndl28@gmail.com>
2024-09-07 11:15:46 +0530

    Matrix

---
+++ TwoDimensionalArraysIntroduction/Qs2_wavyPrintOfMatrix.cpp
@@ -0,0 +1,19 @@
+
+/*for(int col=0;col<m;col++)
+{
+    if(col%2==0)
+    {
+        for(int row=0;row<n;row++)
+        {
+            cout<<matrix[row][col];
+        }
+    }
+    else{
+        for(int row=n-1;row>=0;row--)
+        {
+            cout<<mat[row][col];
+        }
+    }
+
+}
+*/
